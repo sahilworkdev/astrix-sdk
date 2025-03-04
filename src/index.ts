98bf8987aca5de0955dfa01a95693fd0b707f728
sahil <sahil.malik@thewasserstoff.com>
2025-03-04 15:18:09 +0530

    remove unnecc css file

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,3 @@ export { AuthProvider, useAuth } from "./providers/AuthProvider";
 export { formatDateInIST, formatTimeInIST } from "./utils";
 export { default as EventCard } from "./components/eventCard/EventCard";
 export { default as PastEventsWrapper } from "./components/PastEventsWrapper";
-export { default as EventCardCSS } from "./components/eventCard/EventCard.css";
-export { default as EventBannerCSS } from "./components/eventCard/EventBanner.css";
