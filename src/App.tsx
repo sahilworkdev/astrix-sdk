1b539ef563b9cc94646d17cb785e896186d99ee3
rahul roy <rahul.roy@thewasserstoff.com>
2025-03-05 16:39:29 +0530

    add check for tab if no hanlde is provided

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { AuthProvider } from "./providers/AuthProvider";
 import { ThemeProvider } from "./providers/ThemeProvider";
 import "./index.css";
-import Tab from "./components/Tab/Tab";
 
 function App() {
-  const [activeTab, setActiveTab] = useState("Home");
   return (
     <>
       <AuthProvider token={import.meta.env.VITE_ASTRIX_APP_AUTH_TOKEN}>
-        <ThemeProvider accentColor="#3498db">
-          <Tab
-            setActiveTab={setActiveTab}
-            activeTab={activeTab}
-            fontFamily="Poppins"
-            textSize={14}
-            activeTabColor="black"
-          />
-        </ThemeProvider>
+        <ThemeProvider accentColor="#3498db">Hello Astrix</ThemeProvider>
       </AuthProvider>
     </>
   );
