89633a8cb764e7b26932939ac3eb354651431eb9
rahul roy <rahul.roy@thewasserstoff.com>
2025-03-06 15:16:10 +0530

    add capusule component

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,18 +1,5 @@
-import React, { createContext, useContext, ReactNode } from "react";
-
-interface ThemeContextType {
-  accentColor: string;
-}
-
-const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
-
-export const useTheme = (): ThemeContextType => {
-  const context = useContext(ThemeContext);
-  if (!context) {
-    throw new Error("useTheme must be used within a ThemeProvider");
-  }
-  return context;
-};
+import React, { ReactNode } from "react";
+import { ThemeContext } from "./types";
 
 interface ThemeProviderProps {
   accentColor: string;
